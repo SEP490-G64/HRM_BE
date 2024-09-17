ef7669fcb1f6afa3046fd2ea9b968522da790c81
Chu-duong <demonss0910@gmail.com>
2024-09-17 21:21:25 +0700

    fix mvn test build failed

--- src/test/java/com/example/hrm_be/common/HrmBeApplicationTests.java
+++ src/test/java/com/example/hrm_be/common/HrmBeApplicationTests.java
@@ -1,13 +1,20 @@
 package com.example.hrm_be.common;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@Testcontainers
 class HrmBeApplicationTests {
+  @Container
+  public static PostgreSQLContainer<TestcontainersConfiguration> postgreSQLContainer =
+      TestcontainersConfiguration.getInstance();
 
   @Test
-  void contextLoads() {}
+  void contextLoads() {
+  }
 }
