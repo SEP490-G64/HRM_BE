8b66e665c12e9aa533095a118419176c378d6e58
hoanghatieutruc <hoanghatieutruc@gmail.com>
2024-10-03 18:52:15 +0700

    Update BranchType.java

--- src/main/java/com/example/hrm_be/commons/enums/BranchType.java
+++ src/main/java/com/example/hrm_be/commons/enums/BranchType.java
@@ -1,6 +1,28 @@
 package com.example.hrm_be.commons.enums;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum BranchType {
-  MAIN,
-  SUB
+  MAIN("Trụ sở chính"),
+  SUB("Chi nhánh");
+
+  private final String value;
+
+  BranchType(String value) {
+    this.value = value;
+  }
+
+  @JsonValue
+  public String getValue() {
+    return value;
+  }
+
+  public static BranchType fromValue(String value) {
+    for (BranchType type : BranchType.values()) {
+      if (type.value.equalsIgnoreCase(value)) {
+        return type;
+      }
+    }
+    throw new IllegalArgumentException("Unknown value: " + value);
+  }
 }
