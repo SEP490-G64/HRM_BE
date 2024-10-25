7a8991c0e0ffbe584930ccb59a4203c2ff23177a
Chu-duong <demonss0910@gmail.com>
2024-10-25 11:49:54 +0700

    update shouldInitAdminUser test

--- src/test/java/com/example/hrm_be/models/entities/UserEntityTest.java
+++ src/test/java/com/example/hrm_be/models/entities/UserEntityTest.java
@@ -44,14 +44,7 @@ class UserEntityTest {
     assertNotNull(adminUserEntity.getId());
     assertNotNull(adminUserEntity.getUserRoleMap());
     // admin has ROLE_ADMIN and ROLE_USER
-    assertEquals(3, adminUserEntity.getUserRoleMap().size());
-    // admin should have user role
-    assertNotNull(
-        adminUserEntity.getUserRoleMap().stream()
-            .filter(Objects::nonNull)
-            .filter(urm -> urm.getRole() != null)
-            .filter(urm -> urm.getRole().getType().isStaff())
-            .findFirst());
+    assertEquals(1, adminUserEntity.getUserRoleMap().size());
     // admin should have admin role
     assertNotNull(
         adminUserEntity.getUserRoleMap().stream()
