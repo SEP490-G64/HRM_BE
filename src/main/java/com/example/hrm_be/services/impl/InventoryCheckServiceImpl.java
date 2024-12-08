25fb366bb979d16db882f2e096aa5879658dc94e
Chu-duong <demonss0910@gmail.com>
2024-12-08 23:26:07 +0700

    add log

--- src/main/java/com/example/hrm_be/services/impl/InventoryCheckServiceImpl.java
+++ src/main/java/com/example/hrm_be/services/impl/InventoryCheckServiceImpl.java
@@ -638,12 +638,13 @@ public class InventoryCheckServiceImpl implements InventoryCheckService {
     // Broadcast the update via WebSocket to clients
     inventoryChecks.forEach(inventoryCheck -> {
       Long inventoryCheckId = inventoryCheck.getId();
-
+      log.info("before sending payload");
       // Broadcast the message to a specific WebSocket destination
       messagingTemplate.convertAndSend(
           "/topic/inventory-check/" + inventoryCheckId, // Topic for this inventoryCheck
           updatePayload // Payload to send
       );
+      log.info("after sending payload");
     });
   }
 
