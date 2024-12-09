35659904bbb41afc3609e792601f4243ac6f3415
Chu-duong <demonss0910@gmail.com>
2024-12-09 14:22:53 +0700

    add ping pong

--- src/main/java/com/example/hrm_be/configs/WebSocketConfig.java
+++ src/main/java/com/example/hrm_be/configs/WebSocketConfig.java
@@ -6,11 +6,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
   @Autowired private SecurityProperties securityProperties;
   String[] allowedOrigins = {
     "http://localhost:3000",
---
+++ src/main/java/com/example/hrm_be/controllers/WebsocketController.java
@@ -0,0 +1,33 @@
+package com.example.hrm_be.controllers;
+
+import java.util.HashMap;
+import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+@Slf4j
+@Controller
+public class WebsocketController {
+  private final SimpMessagingTemplate messagingTemplate;
+
+  public WebsocketController(SimpMessagingTemplate messagingTemplate) {
+    this.messagingTemplate = messagingTemplate;
+  }
+
+  @MessageMapping("/inventory-check/{inventoryCheckId}")
+  public void handleMessage(@DestinationVariable String inventoryCheckId, @Payload String message) {
+    log.info("Received message for inventoryCheckId " + inventoryCheckId + ": " + message);
+    // Prepare JSON response
+    Map<String, Object> response = new HashMap<>();
+    response.put("status", "success");
+    response.put("message", "Update received for inventoryCheckId: " + inventoryCheckId);
+    response.put("timestamp", System.currentTimeMillis());
+    // Handle the message here
+    messagingTemplate.convertAndSend("/topic/inventory-check/" + inventoryCheckId, response);
+  }
+
+}
